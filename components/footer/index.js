a77d6090fdb123c60b9cf37d15f148a3731ebf02
Ernani Ávila <ernanicavilaneto@gmail.com>
2022-06-08 16:05:57 -0300

    changes into footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -5,44 +5,41 @@ class Footer extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <style>
-      .socialFooterDiv {
-        display: flex;
-        width: 80px;
-        justify-content: space-between;
-        margin: auto;
-      }
-        footer {
-          height: 60px;
-          list-style: none;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          background-color: #dfdfe2;
-          position: relative;
-          width:100%;
-          bottom:0px;
-        }
-             
-        a {
-          margin: 0 15px;
-          color: inherit;
-          text-decoration: none;
-        }
-        
-        a:hover {
-          padding-bottom: 5px;
-          box-shadow: inset 0 -2px 0 0 #333;
-        }
-        
-        .social-row {
-          font-size: 20px;
-        }
-        
-     
-      </style>
-      <footer>
-      <div class="socialFooterDiv">
+    <style>
+    .socialFooterDiv {
+      display: flex;
+      justify-content: space-between;
+      margin: auto;
+    }
+    footer {
+      height: 60px;
+      list-style: none;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #dfdfe2;
+      position: relative;
+      width: 100%;
+      bottom: 0px;
+    }
+
+    a {
+      margin: 0 15px;
+      color: inherit;
+      text-decoration: none;
+    }
+
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #333;
+    }
+
+    .social-row {
+      font-size: 20px;
+    }
+  </style>
+  <footer>
+    <div class="socialFooterDiv">
       <a href="https://www.linkedin.com/in/ernaniavila/"
         ><img src="images/linkedin.png"
       /></a>
@@ -50,7 +47,7 @@ class Footer extends HTMLElement {
         ><img src="images/git.png" width="30px" alt=""
       /></a>
     </div>
-      </footer>
+  </footer>
     `;
   }
 }
